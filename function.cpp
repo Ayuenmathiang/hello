a5f79dbe43a32636629f6e578013951168d4e026
Daniel Ayuen <ayuenmathiang5@gmail.com>
2025-09-06 16:47:24 +0200

    committing all the files to git repository

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int add(int x, int y){
+
+     return x*y;
+}
+int main(){
+    int c=add(5,4);
+    cout<<c<<endl;
+
+    cout<<"hello world mach and Dan";
+}
